7c10439dde8b3d38b5971bd08413b0e8aaa3449b
Imran Khan <imran.4.khan@gm.com>
2024-07-15 02:31:22 +0530

    Selenium Updates

--- src/test/java/Assignment.java
+++ src/test/java/Assignment.java
@@ -9,6 +9,7 @@ import org.testng.Assert;
 public class Assignment {
     public static void main(String[] args) {
         WebDriver driver=new ChromeDriver();
+        driver.manage().window().maximize();
         driver.navigate().to("https://rahulshettyacademy.com/AutomationPractice/");
         WebElement checkbox=driver.findElement(By.id("checkBoxOption1"));
 
@@ -29,8 +30,8 @@ public class Assignment {
         // Navigate to a new URL
         driver.get("https://rahulshettyacademy.com/angularpractice/");
         jse.executeScript("window.scrollBy(0,1000)");
-        driver.findElement(By.cssSelector("input[name='name']")).sendKeys("Imran");
-        driver.findElement(By.cssSelector("input[name='email']")).sendKeys("Imran@gmail.com");
+        driver.findElement(By.name("name")).sendKeys("Imran");
+        driver.findElement(By.name("email")).sendKeys("Imran@gmail.com");
         driver.findElement(By.id("exampleInputPassword1")).sendKeys("Imran@gmail.com");
         driver.findElement(By.id("exampleCheck1")).click();
         WebElement dropdown=driver.findElement(By.id("exampleFormControlSelect1"));
@@ -39,10 +40,12 @@ public class Assignment {
         options.selectByVisibleText("Male");
 
         driver.findElement(By.id("inlineRadio1")).click();
-        driver.findElement(By.cssSelector("input[name='bday']")).sendKeys("02/10/1998");
+        driver.findElement(By.name("bday")).sendKeys("02/10/1998");
 
         driver.findElement(By.cssSelector(".btn.btn-success")).click();
         Assert.assertTrue(driver.findElement(By.cssSelector(".alert:nth-child(1)")).getText().contains(" The Form has been submitted successfully!."));
+
+        driver.quit();
 
     }
 }
