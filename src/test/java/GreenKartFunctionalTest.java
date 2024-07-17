a410653a700641a7b0cb08bccf2569a91d5998a0
Imran Khan <imran.4.khan@gm.com>
2024-07-17 08:55:50 +0530

    Selenium Updates

--- src/test/java/GreenKartFunctionalTest.java
+++ src/test/java/GreenKartFunctionalTest.java
@@ -2,13 +2,20 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 
+import java.time.Duration;
 import java.util.List;
 
-public class FunctionalTest {
+public class GreenKartFunctionalTest {
     public static void main(String[] args) {
         WebDriver driver=new FirefoxDriver();
         String[] item_names={"Carrot","Beans","Walnuts"};
+
+        //driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10)); //Implicit wait
+        WebDriverWait wait=new WebDriverWait(driver,Duration.ofSeconds(8));
 
         driver.get("https://rahulshettyacademy.com/seleniumPractise/#/");
         List<WebElement> productNames=driver.findElements(By.cssSelector("h4.product-name"));
@@ -29,6 +36,17 @@ public class FunctionalTest {
             if(count== item_names.length)
                 break;
         }
+
+        driver.findElement(By.cssSelector("a.cart-icon img")).click();
+        driver.findElement(By.cssSelector("div[class='cart-preview active'] [type='button']")).click();
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("promoCode")));
+        driver.findElement(By.className("promoCode")).sendKeys("rahulshettyacademy");
+        driver.findElement(By.className("promoBtn")).click();
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("promoInfo")));
+        Assert.assertEquals(driver.findElement(By.className("promoInfo")).getText(), "Code applied ..!");
+
         driver.quit();
     }
 }
